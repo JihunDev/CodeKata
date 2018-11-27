a10fdb17807e4576641d030866d2d7ad6ced61de
Ji-Hun Kim <Jihun.dev@gmail.com>
2018-11-28 00:10:55 +0900

    Add : C - YouCantCodeUnderPressure1

---
+++ Kata/8kyu/C/YouCantCodeUnderPressure1.c
@@ -0,0 +1,22 @@
+/*
+    Instruction
+
+    Code as fast as you can! You need to double the integer and return it.
+*/
+/*
+    Sample Tests
+
+    #include <criterion/criterion.h>
+    #include <stdint.h>
+    int32_t double_integer(int32_t);
+
+    Test(sample_test, should_double_integer) {
+      cr_assert_eq(double_integer(2), 4);
+    }
+*/
+
+#include <stdint.h>
+
+int32_t double_integer(int32_t num) {
+  return num * 2;
+}
